c121afc7b39b6719ff56ed63d8f9ed8d313f2d79
AMTdev Alien <craig@amtdev.com>
2018-01-16 15:05:04 -0500

    added template params to app::ssl vhost - added ssl recipe implementation

--- recipes/ssl.rb
+++ recipes/ssl.rb
@@ -16,3 +16,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+execute "disable default site" do
+  command "sudo a2dissite 000-default.conf"
+end
+
+cookbook_file "/etc/ssl/certs/" + node['phpapp']['cert_name'] + ".crt" do
+  backup false
+  action :create
+end
+
+cookbook_file "/etc/ssl/private/" + node['phpapp']['key_name'] + ".key" do
+  owner "root"
+  group "ssl-cert"
+  mode "0640"
+  backup false
+  action :create
+end
+
+template "/etc/apache2/sites-available/000-" + node['app']['server_name'] + "-ssl.conf" do
+  source 'site.conf.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  variables(
+    :docroot        	 =>  node['app']['path'] + "/" + node['app']['project_dir'] + "/" + node['app']['public_dir'],
+    :server_port        	 => "80",
+    :server_name     => node["app"]["server_name"],
+    :server_alias      => node["app"]["server_alias"],
+    :cert_name  =>  node["app"]["cert_name"],
+    :key_name  =>  node["app"]["key_name"],
+    :directory_options        	 => "Indexes FollowSymLinks",
+    :allow_override        	 => "All")
+end
+
+execute "enable site" do
+  command "sudo a2ensite 000-" + node['app']['server_name'] + ".conf"
+end
+
+execute "restart apache2" do
+  command "sudo apache2ctl restart"
+end
