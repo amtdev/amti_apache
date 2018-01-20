63547412019bd37d0f4c966d6cf6eef3eeeca16e
Flavio-PC <craig@amtdev.com>
2018-01-20 00:21:24 +0000

    added app2 ssl recipe implementation

--- recipes/app2_ssl.rb
+++ recipes/app2_ssl.rb
@@ -16,3 +16,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+template "/etc/apache2/sites-available/111-" + node['app2']['server_name'] + "-ssl.conf" do
+  source 'site.conf.ssl.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+  variables(
+    :docroot        	 =>  node['app2']['path'] + "/" + node['app2']['project_dir'] + "/" + node['app2']['public_dir'],
+    :server_port        	 => "443",
+    :server_name     => node["app2"]["server_name"],
+    :server_alias      => node["app2"]["server_alias"],
+    :cert_name  =>  node["app2"]["cert_name"],
+    :key_name  =>  node["app2"]["key_name"],
+    :directory_options        	 => "Indexes FollowSymLinks",
+    :allow_override        	 => "All")
+end
+
+execute "enable site" do
+  command "sudo a2ensite 111-" + node['app2']['server_name'] + "-ssl.conf"
+end
+
+service "apache2" do
+  action :restart
+end
